d6fc8d4212ff1af2b5deb97fee09eb2e27501a42
jonahglover <jonah@jonahglover.com>
2014-05-10 13:18:20 -0400

    - added comment submission

--- assets/javascripts/app/controllers.js
+++ assets/javascripts/app/controllers.js
@@ -3,10 +3,25 @@ app.controller('mainController', ['$scope', 'PALACE', function($scope, PALACE) {
     $scope.init = function(){
         $scope.comments = [];
         loadComments();
+        $scope.comment = "Say Something!";
     }
 
-    $scope.addComment = function(){
-        
+    $scope.submitComment = function(){
+        $scope.isUploading = true;
+        console.log($scope.comment);
+        palace_data = {
+            "type"      :"send",
+            "subdomain" :"demos",
+            "api"       :"mysore",
+            "key"       :"mysore",
+            "data"      :[{"name":"Test Name","email":"jdeglover@aol.com","comment":"Hey Dude! Cool Site!"}]
+        };
+
+        PALACE
+        .send(palace_data)
+        .success(function(data){
+            $scope.isUploading = false;
+        });
     }
 
     loadComments = function(){
